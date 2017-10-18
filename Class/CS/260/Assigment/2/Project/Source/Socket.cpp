12703afce69769cbb70a953627fcc451ebf860b1
Underdisc <connortdeakin@gmail.com>
2017-10-18 22:08:00 +0100

    CS 260 Assignment 2 fin

---
+++ Class/CS/260/Assigment/2/Project/Source/Socket.cpp
@@ -0,0 +1,126 @@
+/* All content(c) 2017 - 2018 DigiPen(USA) Corporation, all rights reserved. */
+/*****************************************************************************/
+/*!
+\file Socket.cpp
+\author Connor Deakin
+\par E-mail: connor.deakin\@digipen.edu
+\par Project: CS 260
+\date 2017/10/17
+\brief
+  Contains the implementation of the SocketTCP class.
+*/
+/*****************************************************************************/
+
+#ifdef _WIN32
+#include <Ws2tcpip.h>
+#else
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#endif
+
+#include "Socket.h"
+
+
+#ifndef _WIN32
+#define SOCKET_ERROR -1
+#endif
+
+SocketTCP::SocketTCP(int port, char * ipv4_address) :
+_addressSize(sizeof(sockaddr_in)), _block(true)
+{
+  _socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  _address.sin_family = AF_INET;
+  _address.sin_port = htons(port); // host to network short
+  #ifdef _WIN32
+  InetPton(AF_INET, ipv4_address, &_address.sin_addr);
+  #else
+  inet_pton(AF_INET, ipv4_address, &_address.sin_addr);
+  #endif
+}
+
+void SocketTCP::Connect()
+{
+  int result = connect(_socket, (sockaddr *)&_address, _addressSize);
+  ErrorCheck(result);
+}
+
+int SocketTCP::Send(char * data, int bytes)
+{
+  int result = send(_socket, data, bytes, 0);
+  if (result == SOCKET_ERROR) {
+    #ifdef _WIN32
+    int error = WSAGetLastError();
+    if (error == WSAEWOULDBLOCK)
+      return SEND_SOCKET_FULL;
+    #else
+    if (errno == EWOULDBLOCK)
+     return SEND_SOCKET_FULL;
+    #endif
+    // other error occured
+  }
+  return result;
+}
+
+int SocketTCP::Recieve(char * data, int bytes)
+{
+  // do error checking
+  int result = recv(_socket, data, bytes, 0);
+  if (result == SOCKET_ERROR) {
+    #ifdef _WIN32
+    int error = WSAGetLastError();
+    if (error == WSAEWOULDBLOCK)
+      return RECIEVE_NO_DATA;
+    #else
+    if (errno == EWOULDBLOCK)
+     return RECIEVE_NO_DATA;
+    #endif
+    // other error occured
+  }
+  return result;
+}
+
+void SocketTCP::Block(bool block)
+{
+  if (_block == block)
+    return;
+  #ifdef _WIN32
+  unsigned long to_block = block ? 0 : 1;
+  int result = ioctlsocket(_socket, FIONBIO, &to_block);
+  #else
+  int flags = fcntl(_socket, F_GETFL, 0);
+  flags = block ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
+  int result = fcntl(_socket, F_SETFL, flags);
+  #endif
+  ErrorCheck(result);
+  _block = block;
+}
+
+void SocketTCP::CloseRecieve()
+{
+  #ifdef _WIN32
+  int result = shutdown(_socket, SD_RECEIVE);
+  #else
+  int result = shutdown(_socket, SHUT_RD);
+  #endif
+  ErrorCheck(result);
+}
+
+void SocketTCP::CloseSend()
+{
+  #ifdef _WIN32
+  int result = shutdown(_socket, SD_SEND);
+  #else
+  int result = shutdown(_socket, SHUT_WR);
+  #endif
+  ErrorCheck(result);
+}
+
+int SocketTCP::ErrorCheck(int return_value)
+{
+  if (return_value == SOCKET_ERROR) {
+    // error occured
+    // check WSAGetLastError
+  }
+  return 0;
+}
---
+++ Class/CS/260/Assigment/2/Share/Project/Source/Socket.h
@@ -0,0 +1,61 @@
+/* All content(c) 2017 - 2018 DigiPen(USA) Corporation, all rights reserved. */
+/*****************************************************************************/
+/*!
+\file Socket.h
+\author Connor Deakin
+\par E-mail: connor.deakin\@digipen.edu
+\par Project: CS 260
+\date 2017/10/17
+\brief
+  Contains the interface for the SocketTCP class. Used for creating and
+  managing a TCP socket.
+*/
+/*****************************************************************************/
+
+#ifndef SOCKETTCP_H
+#define SOCKETTCP_H
+
+#ifdef _WIN32
+#include <WinSock2.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#endif
+
+#ifndef _WIN32
+typedef int SOCKET;
+#endif
+
+#define MTU_SIZE 1500
+
+#define SEND_SOCKET_FULL -1
+#define RECIEVE_NO_DATA -1
+#define RECIEVE_FIN 0
+
+/*****************************************************************************/
+/*!
+\class SocketTCP
+\brief
+  Used for TCP socket creation and management.
+*/
+/*****************************************************************************/
+class SocketTCP
+{
+public:
+  SocketTCP(int port, char * ipv4_address);
+  void Connect();
+  int Send(char * data, int bytes);
+  int Recieve(char * data, int bytes);
+  void Block(bool block);
+  void CloseRecieve();
+  void CloseSend();
+private:
+  SOCKET _socket;
+  sockaddr_in _address;
+  int _addressSize;
+  bool _block;
+
+  int ErrorCheck(int return_value);
+};
+
+#endif // !SOCKETTCP_H
