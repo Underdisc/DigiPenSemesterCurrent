3cd98db57edcf39378584046b186b0d6bef9b7f4
Underdisc <connortdeakin@gmail.com>
2017-12-11 13:21:32 +0000

    Brute Force Knapsack

--- Class/CS/330/Assignment/6/knapsack_brute_force_minchange.h
+++ Class/CS/330/Assignment/6/knapsack_brute_force_minchange.h
@@ -1,20 +1,38 @@
+/*****************************************************************************/
+/*!
+\file knapsack_brute_force_minchange..h
+\author Connor Deakin
+\par E-mail: connortdeakin\@gmail.com
+\par Project: Knapsack Brute Force
+\date 11/15/2017
+\brief
+  Contains the interface for a Grey Code generator and a brute force knapsack
+  implementation.
+*/
+/*****************************************************************************/
+
 #ifndef KNAPSACK_BRUTE_FORCE_MINCHANGE
 #define KNAPSACK_BRUTE_FORCE_MINCHANGE
-#include <utility>   // std::pair
+
+#include <utility>
 #include <vector>
 #include <bitset>
 #include "definitions.h"
 
 class GreyCode {
-    private:
-
-    public:
-        GreyCode( int s );
-        // first subset is empty, this function tells what's next 2^n-1 subsets
-        // ret value: is last, is add, positin
-        std::pair< bool, std::pair< bool, int > > Next();
+  private:
+    unsigned long long _numPermutations;
+    unsigned long long _currentIteration;
+    std::vector<bool> _currentGreyCode;
+    int FindPositionToChange(unsigned long long next_iteration);
+  public:
+    GreyCode( int s );
+    std::pair< bool, std::pair< bool, int > > Next();
+    const std::vector<bool> & GetCurrentGreyCode();
 };
 
 // brute-force knapsack
-std::vector<bool> knapsack_brute_force( std::vector<Item> const& items, Weight const& W );
-#endif
+std::vector<bool> knapsack_brute_force(std::vector<Item> const& items,
+  Weight const& W);
+
+#endif //!KNAPSACK_BRUTE_FORCE_MINCHANGE
