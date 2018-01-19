8b1d89f3dfbca0629235edb83f3ed427c86812e4
Underdisc <connortdeakin@gmail.com>
2018-01-19 07:42:26 +0000

    Started CS 350 Assignment 1

--- Class/CS/350/Assignment/Project/CS350Framework/AssignmentFiles/Geometry.cpp
+++ Class/CS/350/Assignment/Project/CS350Framework/AssignmentFiles/Geometry.cpp
@@ -7,20 +7,62 @@
 #include "Precompiled.hpp"
 
 
+bool InRange(float min, float max, float value)
+{
+  return value > min && value < max;
+}
+
 bool BarycentricCoordinates(const Vector3& point, const Vector3& a, const Vector3& b,
                             float& u, float& v, float expansionEpsilon)
 {
-  /******Student:Assignment1******/
-  Warn("Assignment1: Required function un-implemented");
-  return false;
+  // b -> p and b -> a vector calcuations
+  Vector3 b_p = point - b;
+  Vector3 b_a = a - b;
+  // calculation for u
+  float denom = Math::Dot(b_a, b_a);
+  if (denom == 0.0f) {
+    u = 0.0f;
+    v = 0.0f;
+    return false;
+  }
+  float numer = Math::Dot(b_p, b_a);
+  u = numer / denom;
+  v = 1.0f - u;
+  // see if the point is on the line segment
+  float min = 0.0f - expansionEpsilon;
+  float max = 1.0f + expansionEpsilon;
+  return InRange(min, max, u);
 }
 
 bool BarycentricCoordinates(const Vector3& point, const Vector3& a, const Vector3& b, const Vector3& c,
                             float& u, float& v, float& w, float expansionEpsilon)
 {
-  /******Student:Assignment1******/
-  Warn("Assignment1: Required function un-implemented");
-  return false;
+  // calculate cross products for u and v
+  Vector3 Npbc = Math::Cross(b - point, c - point);
+  Vector3 Npca = Math::Cross(c - point, a - point);
+  Vector3 Nabc = Math::Cross(b - a, c - a);
+  // calculate u, v and w
+  float denom = Math::Dot(Nabc, Nabc);
+  if (denom == 0.0f) {
+    // degenerate triangle
+    u = 0.0f;
+    v = 0.0f;
+    w = 0.0f;
+    return false;
+  }
+  float u_numer = Math::Dot(Npbc, Nabc);
+  float v_numer = Math::Dot(Npca, Nabc);
+  u = u_numer / denom;
+  v = v_numer / denom;
+  w = 1.0f - u - v;
+  // see if the point is in the triangle
+  float min = 0.0f - expansionEpsilon;
+  float max = 1.0f + expansionEpsilon;
+  bool u_in_range = InRange(min, max, u);
+  bool v_in_range = InRange(min, max, v);
+  bool w_in_range = InRange(min, max, w);
+  return u_in_range && v_in_range && w_in_range;
+
 }
 
 IntersectionType::Type PointPlane(const Vector3& point, const Vector4& plane, float epsilon)
--- Class/CS/350/Assignment/Project/CS350Framework/AssignmentFiles/Shapes.cpp
+++ Class/CS/350/Assignment/Project/CS350Framework/AssignmentFiles/Shapes.cpp
@@ -315,16 +315,15 @@ void Plane::Set(const Triangle& triangle)
 
 void Plane::Set(const Vector3& p0, const Vector3& p1, const Vector3& p2)
 {
-  /******Student:Assignment1******/
-  // Set mData from the 3 points. Note: You should most likely normalize the plane normal.
-  Warn("Assignment1: Required function un-implemented");
+  Set(Math::Cross(p1 - p0, p2 - p0), p0);
 }
 
 void Plane::Set(const Vector3& normal, const Vector3& point)
 {
-  /******Student:Assignment1******/
-  // Set mData from the normal and point. Note: You should most likely normalize the plane normal.
-  Warn("Assignment1: Required function un-implemented");
+  float normal_mag = normal.Length();
+  for (int i = 0; i < 3; ++i)
+    mData[i] = normal[i] / normal_mag;
+  mData[4] = Math::Dot(normal, point);
 }
 
 Vector3 Plane::GetNormal() const
