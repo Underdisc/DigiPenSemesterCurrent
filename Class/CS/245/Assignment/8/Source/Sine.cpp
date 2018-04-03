1f6e6cf767dc94f307b9f83b536ca144cbc83422
Underdisc <connortdeakin@gmail.com>
2018-04-03 21:17:10 +0100

    uploading files

---
+++ Class/CS/245/Assignment/8/Source/Sine.cpp
@@ -0,0 +1,72 @@
+/*****************************************************************************/
+/*!
+\file Sine.cpp
+\author Connor Deakin
+\par E-mail: connortdeakin\@gmail.com
+\par Assignment: 8
+\par Course: CS 245
+\par Term: Spring 2018
+\date 26/03/2018
+\brief
+  Contains the implementation of the sine lookup table.
+*/
+/*****************************************************************************/
+
+#include <stdexcept>
+#include "Sine.h"
+
+#define TAU 6.28318531
+#define TAUF 6.28318531f
+
+Sine::Sine(unsigned R)
+{
+  sine_table.resize(R);
+  // calculate initial values
+  // yn = beta * yn-1 - yn-2
+  double yn_2 = 0.0; // yn-2
+  double yn_1 = std::sin(TAU / (double)R); // yn-1
+  double beta = 2.0 * std::cos(TAU / (double)R);
+  // initialize first elements
+  if(R >= 2)
+  {
+    sine_table[0] = 0.0f;
+    sine_table[1] = (float)yn_1;
+  }
+  // throw error if less than two samples are requested
+  // this would defeat the purpose of this class
+  else
+  {
+    std::runtime_error error("R should be at least greater than 2");
+    throw(error);
+  }
+  // compute remaining table values
+  for(unsigned i = 2; i < R; ++i)
+  {
+    double yn = beta * yn_1 - yn_2;
+    sine_table[i] = (float)yn;
+    yn_2 = yn_1;
+    yn_1 = yn;
+  }
+}
+
+float Sine::operator()(float t)
+{
+  const size_t R = sine_table.size();
+  // find fractional index into table
+  float index = (t * (float)R) / TAUF;
+  // seperate into integer and fractional part
+  int int_part = (int)index;
+  float fract_part = index - (float)int_part;
+  // bring int_part into range [0, R)
+  int_part = int_part % R;
+  if(int_part < 0)
+  {
+    int_part += R;
+    fract_part += 1.0f;
+  }
+  // calculate the value of sine using the lookup table
+  float sample0 = sine_table[int_part];
+  float sample1 = sine_table[(int_part + 1) % R];
+  float final_sample = sample0 + fract_part * (sample1 - sample0);
+  return final_sample;
+}
---
+++ Class/CS/355/Assignment/2_lfsv/assignment/lfsv-skeleton.h
@@ -0,0 +1,98 @@
+#include <iostream>       // std::cout
+#include <atomic>         // std::atomic
+#include <thread>         // std::thread
+#include <vector>         // std::vector
+#include <deque>          // std::deque
+#include <mutex>          // std::mutex
+#include <cstring>
+
+template< typename T, int NUMBER, unsigned SIZE >
+class MemoryBank {
+    std::deque< T* > slots;
+    std::mutex m;
+    public:
+        MemoryBank() : slots(NUMBER) {
+            for ( int i=0; i<NUMBER; ++i ) {
+                slots[i] = reinterpret_cast<T*>( new char[ SIZE ] );
+            }
+        }
+        T* Get() {
+            std::lock_guard<std::mutex> lock( m );
+            T* p = slots[0];
+            slots.pop_front();
+            return p;
+        }
+        void Store( T* p ) {
+            std::memset( p, 0, SIZE ); // clear data
+            std::lock_guard<std::mutex> lock( m );
+            slots.push_back( p );
+        }
+        ~MemoryBank() {
+            for ( auto & el : slots ) { delete [] reinterpret_cast<char*>( el ); }
+        }
+};
+
+
+struct Pair {
+    int*    pointer;
+    int     size;
+}; //  __attribute__((aligned(16),packed)); // bug in GCC 4.*, fixed in 5.1?
+// alignment needed to stop std::atomic<Pair>::load to segfault
+
+class LFSV {
+//    MemoryBank<std::vector<int>, 290000, sizeof(void*) > mb;
+    MemoryBank<int, 2000, sizeof(int[10000]) > mb2;
+    std::atomic< Pair > pdata;
+    public:
+
+    LFSV() : mb2(), pdata( Pair{ mb2.Get(), 0 } ) {
+    }   
+
+    ~LFSV() { 
+        Pair temp = pdata.load();
+        int* p = temp.pointer;
+        if ( p != nullptr ) {
+//            p->~vector();
+            mb2.Store( p );
+        }
+    }
+
+    void Insert( int const & v ) {
+        Pair pdata_new, pdata_old;
+        pdata_new.pointer  = nullptr;
+        do {
+            //delete pdata_new.pointer;
+            if ( pdata_new.pointer != nullptr ) { 
+//                pdata_new.pointer->~vector();
+                mb2.Store( pdata_new.pointer ); 
+            }
+            pdata_old = pdata.load();
+
+            pdata_new.size = pdata_old.size;
+//            pdata_new.pointer   = new (mb.Get()) std::vector<int>( *pdata_old.pointer );
+            pdata_new.pointer   = mb2.Get();
+            std::memcpy( pdata_new.pointer, pdata_old.pointer, pdata_new.size*sizeof(int) );
+            
+            int * a = pdata_new.pointer;
+            a[pdata_new.size]=v; // add new value in the end
+
+            for ( int i=0; i<pdata_new.size; ++i ) {
+                if ( a[i]>=v ) {
+                    for ( int j=pdata_new.size; j>i; --j ) {
+                        std::swap( a[j], a[j-1] ); // move new element to proper position
+                    }
+                    break;
+                }
+            }
+            ++pdata_new.size; // set new size
+        } while ( !(this->pdata).compare_exchange_strong( pdata_old, pdata_new  ));
+        //delete pdata_old.pointer;
+        //pdata_old.pointer->~vector();
+        mb2.Store( pdata_old.pointer );
+    }
+
+    int operator[] ( int pos ) { // not a const method anymore
+        int ret_val = pdata.load().pointer[pos]; 
+        return ret_val;
+    }
+};
