991ee448d7a5791b620e8ade79119bfa6f3ac6ae
Underdisc <connortdeakin@gmail.com>
2018-09-28 10:15:22 +0100

    mat320 proj 1 p

---
+++ class/mat/320/project/1/src/Complex.h
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////////
+/// @file Complex.h
+/// @author Connor Deakin
+/// @email connor.deakin@digipen.edu
+/// @date 2017-09-21
+///
+/// @brief Interface and implementation for complex number arithmetic.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+class Complex
+{
+public:
+  Complex();
+  Complex(float real, float imaginary);
+  Complex(const Complex & other);
+  float Real();
+  float Imaginary();
+  Complex Conjugate();
+  Complex operator+(const Complex & rhs);
+  Complex operator-(const Complex & rhs);
+  Complex operator*(const Complex & rhs) const;
+  Complex operator*(float rhs) const;
+  Complex & operator+=(const Complex & rhs);
+  Complex & operator-=(const Complex & rhs);
+  Complex & operator*=(const Complex & rhs);
+  Complex & operator*=(float rhs);
+private:
+  union
+  {
+    struct
+    {
+      float m_Real, m_Imaginary;
+    };
+    float m_Values[2];
+  };
+};
+
+#endif // !COMPLEX_H
